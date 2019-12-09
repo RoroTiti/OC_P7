d1861ff3daa0018941a4c18d1cf3259da9f22442
Romaric Thibault <torburma@gmail.com>
2019-12-09 10:56:06 +0100

    First tests with OSM/OMW, to be continued and mocked

---
+++ chatbot/openmediawiki.py
@@ -0,0 +1,6 @@
+class OpenMediaWiki:
+    def __init__(self):
+        pass
+
+    def get_answer(self, question):
+        pass
---
+++ tests/test_openstreetmap.py
@@ -0,0 +1,13 @@
+from chatbot.openstreetmap import OpenStreetMap
+
+
+def test_get_answer():
+    osm = OpenStreetMap()
+    question = "Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?"
+    assert osm.get_answer(question) == "Bien sûr mon poussin ! La voici : 7 cité Paradis, 75010 Paris."
+
+    question = "Salut GrandPy ! Est-ce que tu connais l'adresse du Futuroscope ?"
+    assert osm.get_answer(question) == "Bien sûr mon poussin ! La voici : D 20d, 86360 Chasseneuil-du-Poitou."
+
+    question = "Salut GrandPy ! Est-ce que tu connais l'adresse du Puy du Fou ?"
+    assert osm.get_answer(question) == "Bien sûr mon poussin ! La voici : La Glaneuse, 85590 Les Épesses."
