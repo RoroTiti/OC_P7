757e2735d66471260e71659d66d1b91d22ecd0a2
Romaric Thibault <torburma@gmail.com>
2019-12-13 15:27:16 +0100

    Base GrandPyBot web UI

--- grandpybot/main.py
+++ grandpybot/main.py
@@ -1,10 +1,10 @@
-# from flask import Flask
-# from flask import render_template
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/hello/')
-# @app.route('/hello/<name>')
-# def hello(name=None):
-#     return render_template('hello.html', name=name)
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/<name>')
+def hello(name=None):
+    return render_template('index.html', name=name)
