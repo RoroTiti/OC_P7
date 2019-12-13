ff40c103025b3c458ee7397596a166349a1eed94
Romaric Thibault <torburma@gmail.com>
2019-12-13 15:50:27 +0100

    Design improvements + base app routes

--- grandpybot/main.py
+++ grandpybot/main.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('index.html', name=name)
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/", methods=["POST"])
+def render_question():
+    data = request.form
+    question = data["question"]
+    return render_template("user_question.html", question=question)
+
+
+@app.route("/answer", methods=["POST"])
+def answer_question():
+    pass
